c1d8bbd9b1d6f42cf2482cb8fb1b9a928396ac8a
weizmann <kiend.weizman7@gmail.com>
2022-07-04 12:24:39 -0500

    Update

--- light_curve_condor.py
+++ light_curve_condor.py
@@ -90,23 +90,46 @@ def main():
     fid =  open(args.condor_dag_file, "w")
     fid1 = open(args.bash_file, "w")
     
-    outdir = os.path.join(args.outdir, str(args.binary_type))
-    if not os.path.isdir(outdir):
-        os.makedirs(outdir)    
 
-    outdir = os.path.join(args.outdir, "%d" % ii)
+    for ii in range(injection_num):
+        for exp in exposure:
+            for seed  in seed_list:
+                outdir = os.path.join(args.outdir, f"{args.binary_type}/{ii}_{exp}_{seed}".format())
+                if not os.path.isdir(outdir):
+                    os.makedirs(outdir)  
 
-    fid.write("JOB %d %s\n" % (job_number, args.condor_sub_file))
-    fid.write("RETRY %d 3\n" % (job_number))
-    fid.write(
-        'VARS %d jobNumber="%d" outdir="%s" GWsamples="%s" EMsamples="%s"\n'
-        % (job_number, job_number, outdir, gwsamples, emsamples)
-    )
-    fid.write("\n\n")
-    job_number = job_number + 1
+                fid.write("JOB %d %s\n" % (job_number, args.condor_sub_file))
+                fid.write("RETRY %d 3\n" % (job_number))
+                fid.write(
+                    'VARS %d jobNumber="%d" outdir="%s" GWsamples="%s" EMsamples="%s"\n'
+                    % (job_number, job_number, outdir, gwsamples, emsamples)
+                )
+                fid.write("\n\n")
+                job_number = job_number + 1
 
-   
+
+
+
+                   fid1.write(
+                    "%s --model %s --svd-path /home/%s/gwemlightcurves/output/svdmodels --outdir %s --label injection_%s --prior analysis.prior --tmin 0 --tmax 20 --dt 0.5 --error-budget 1 --nlive %d --Ebv-max 0 --injection %s --injection-num %s --injection-detection-limit %s --injection-outfile %s --generation-seed 42 --filters %s --plot --remove-nondetections --optimal-augmentation --optimal-augmentation-N-points %d --optimal-augmentation-filters %s --optimal-augmentation-seed %d\n"
+                    % (
+                        lc_analysis,
+                        args.model,
+                        os.environ["USER"],
+                        outdir,
+                        args.model,
+                        args.nlive,
+                        args.injection_file,
+                        str(index),
+                        args.injection_detection_limit,
+                        injfile,
+                        args.filters,
+                        args.optimal_augmentation_N_points,
+                        args.optimal_augmentation_filters,
+                        ii,
+                    )
     
+        
     
     nmma_create_injection --prior-file ./{bin_type}.prior --eos-file ./ALF2.dat --binary-type {bin_type} --n-injection {n_inj} --original-parameters --extension json
  
